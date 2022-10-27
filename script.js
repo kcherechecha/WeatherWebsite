f251810b4b79fda62655616da5f60974d10efd24
kcherechecha <115093902+kcherechecha@users.noreply.github.com>
2022-10-27 16:43:54 +0200

    Added JS file

---
+++ script.js
@@ -0,0 +1,60 @@
+// let weather = {
+//   paris: {
+//     temp: 19.7,
+//     humidity: 80,
+//   },
+//   tokyo: {
+//     temp: 17.3,
+//     humidity: 50,
+//   },
+//   lisbon: {
+//     temp: 30.2,
+//     humidity: 20,
+//   },
+//   sanfrancisco: {
+//     temp: 20.9,
+//     humidity: 100,
+//   },
+//   oslo: {
+//     temp: -5,
+//     humidity: 20,
+//   },
+// };
+
+// write your code here
+// weather.paris.rounded = Math.round(weather.paris.temp);
+// weather.tokyo.rounded = Math.round(weather.tokyo.temp);
+// weather.sanfrancisco.rounded = Math.round(weather.sanfrancisco.temp);
+// weather.oslo.rounded = Math.round(weather.oslo.temp);
+// weather.lisbon.rounded = Math.round(weather.lisbon.temp);
+// let strings = ["Paris", "Tokyo", "Lisbon", "San Francisco", "Oslo"];
+// for (var i = 0; i < strings.length; i++) {
+//   strings[i] = strings[i].toLowerCase();
+// }
+// let enter = prompt("Enter a city");
+// enter = enter.trim().toLowerCase();
+// if (enter === strings[0]) {
+//   alert(
+//     `It is currently ${weather.paris.rounded}°C in Paris with a humidity of ${weather.paris.humidity}%`
+//   );
+// } else if (enter === strings[1]) {
+//   alert(
+//     `It is currently ${weather.tokyo.rounded}°C in Tokyo with a humidity of ${weather.tokyo.humidity}%`
+//   );
+// } else if (enter === strings[2]) {
+//   alert(
+//     `It is currently ${weather.lisbon.rounded}°C in Lisbon with a humidity of ${weather.lisbon.humidity}%`
+//   );
+// } else if (enter === strings[3]) {
+//   alert(
+//     `It is currently ${weather.sanfrancisco.rounded}°C in San Francisco with a humidity of ${weather.sanfrancisco.humidity}%`
+//   );
+// } else if (enter === strings[4]) {
+//   alert(
+//     `It is currently ${weather.oslo.rounded}°C in Oslo with a humidity of ${weather.oslo.humidity}%`
+//   );
+// } else {
+//   alert(
+//     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${enter}`
+//   );
+// }
